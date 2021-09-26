be567a5a86e2ec5fe5a47ae89b0db5a4764fc7b1
Matt <mattm458@gmail.com>
2021-09-26 15:25:53 -0400

    Arc function

--- main.py
+++ main.py
@@ -14,8 +14,23 @@ def square(t, size):
 
 def circle(t, radius):
     circumference = radius * 2 * math.pi
-    polygon(bob, 50, circumference / 50)
+    n = int(circumference / 3) + 1
+    length = circumference / n
+    
+    polygon(bob, n, length)
 
-circle(bob, 80)
+def arc(t, radius, angle):
+    circumference = radius * 2 * math.pi
+    arc_length = circumference * angle / 360
+    n = int(arc_length / 3) + 1
+
+    step_length = arc_length / n
+    step_angle = angle / n
+
+    for i in range(n):
+        t.forward(step_length)
+        t.left(step_angle)
+
+arc(bob, 200, 38)
 
 turtle.mainloop()
